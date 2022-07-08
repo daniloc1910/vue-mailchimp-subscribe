a5a664bd34a51157cd48408a70732379819e852f
Danilo Campos <55562946+daniloc1910@users.noreply.github.com>
2022-07-08 10:22:02 +0200

    fields

--- src/vue-mailchimp-subscribe.js
+++ src/vue-mailchimp-subscribe.js
@@ -33,26 +33,12 @@ export default {
       return queryString.stringify({
         u: this.userId,
         id: this.listId,
-<<<<<<< Updated upstream
-        EMAIL: this.email,
-      })
-=======
         ...this.fields,
       });
->>>>>>> Stashed changes
     },
   },
 
   methods: {
-<<<<<<< Updated upstream
-    setEmail(value = '') {
-      this.email = value.trim()
-    },
-
-    subscribe() {
-      if (this.email === null || this.loading) {
-        return
-=======
     // deprecated in favour of `setField('EMAIL', value)`
     setEmail(value = "") {
       this.fields["EMAIL"] = value.trim();
@@ -65,7 +51,6 @@ export default {
     subscribe() {
       if (!this.fields["EMAIL"] || this.loading) {
         return;
->>>>>>> Stashed changes
       }
 
       this.success = false;
@@ -91,15 +76,9 @@ export default {
       if (this.error) {
         this.$emit("error", this.error);
       } else {
-<<<<<<< Updated upstream
-        this.success = true
-        this.email = null
-        this.$emit('success')
-=======
         this.success = true;
         this.fields = {};
         this.$emit("success");
->>>>>>> Stashed changes
       }
     },
 
@@ -117,8 +96,4 @@ export default {
       loading: this.loading,
     });
   },
-<<<<<<< Updated upstream
-}
-=======
 };
->>>>>>> Stashed changes
